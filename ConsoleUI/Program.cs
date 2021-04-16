e3cd9aae983c0ed9cccc40dacce0f55664931ce8
erayyaman5759@gmail.com <erayyaman5759@gmail.com>
2021-04-16 18:18:49 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.Entity_Framework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarsByDailyPrice(20,80))
+            {
+                Console.WriteLine("Car Id :"+car.CarId + "\nCar Name :"+car.Description);
+            }
+            
+        }
+    }
+}
